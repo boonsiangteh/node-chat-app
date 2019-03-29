afca36f9ce15d4901b84e71a677da355cc199e09
boonHanalytics2018 <boonsiang.teh@biomind.ai>
2019-03-29 17:58:29 +0800

    integrated socket io and added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+// manually creating http server
+const server = http.createServer(app);
+// get access to socket io by passing in our http server into socketIO
+const io = socketIO(server);
 
 // create default port or get from env variable
 const port = process.env.PORT || 3005;
@@ -11,10 +17,14 @@ console.log(publicPath);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.render('index.html');
-// });
+// use built-in event listener on socketio to listen to incoming connections
+io.on('connection', (socket) => {
+  console.log('client connected');
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
